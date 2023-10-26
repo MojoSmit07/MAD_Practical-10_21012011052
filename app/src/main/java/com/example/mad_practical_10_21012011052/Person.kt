ac8365d5a0c1735fa85b5596945c8113f83ffbe7
student <student@gnu.ac.in>
2023-10-26 08:24:32 +0530

    Practical-10

---
+++ app/src/main/java/com/example/mad_practical_10_21012011052/Person.kt
@@ -0,0 +1,15 @@
+package com.example.mad_practical_10_21012011052
+
+import java.io.Serializable
+
+    class Person(
+        var id : String,
+        var name : String,
+        var emailid: String,
+        var phoneNo: String,
+        var address: String,
+        var latitude: Double,
+        var longitude: Double) :Serializable {
+
+
+    }
