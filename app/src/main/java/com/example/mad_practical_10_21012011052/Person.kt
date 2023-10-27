11980fb33e27f5fbcb362e5ab48735fb54cfc4c2
Smit Modi <smitmodi21@gnu.ac.in>
2023-10-27 15:04:43 +0530

    Practical-10

--- app/src/main/java/com/example/mad_practical_10_21012011052/Person.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011052/Person.kt
@@ -1,8 +1,9 @@
 package com.example.mad_practical_10_21012011052
 
+import org.json.JSONObject
 import java.io.Serializable
 
-    class Person(
+class Person(
         var id : String,
         var name : String,
         var emailid: String,
@@ -11,5 +12,16 @@ import java.io.Serializable
         var latitude: Double,
         var longitude: Double) :Serializable {
 
+    constructor(jsonObject:JSONObject):this("","","","","",0.0,0.0) {
+        id = jsonObject.getString("id")
+        emailid = jsonObject.getString("email")
+        phoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        name = profileJson.getString("name")
+        address = profileJson.getString("address")
+        val locationJson = profileJson.getJSONObject("location")
+        latitude = locationJson.getDouble("lat")
+        longitude = locationJson.getDouble("long")
+    }
     }
 
