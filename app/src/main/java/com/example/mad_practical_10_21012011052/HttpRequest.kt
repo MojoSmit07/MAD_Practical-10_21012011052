dd0ba5233fd3fda4e0abf37646c937ef49abd9e9
student <student@gnu.ac.in>
2023-10-28 09:19:12 +0530

    practical-10

--- app/src/main/java/com/example/mad_practical_10_21012011052/HttpRequest.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011052/HttpRequest.kt
@@ -1,21 +1,55 @@
 package com.example.mad_practical_10_21012011052
 
-import org.json.JSONArray
-import org.json.JSONObject
+import android.util.Log
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.ProtocolException
+import java.net.URL
 
 class HttpRequest {
-    private fun getPersonDetailsFromJson(sJson: String?) {
-        val personList = ArrayList<Person>()
+    private val TAG = "HttpRequest"
+    fun makeServiceCall(reqUrl: String?,token:String?=null): String? {
+        var response: String? = null
         try {
-            val jsonArray = JSONArray(sJson)
-            for (i in 0 until jsonArray.length()) {
-                val jsonObject = jsonArray[i] as JSONObject
-                val person = Person(jsonObject)
-                personList.add(person)
+            val url = URL(reqUrl)
+            val conn = url.openConnection() as HttpURLConnection
+            if(token != null){
+                conn.setRequestProperty("Authorization","Bearer $token");
+                conn.setRequestProperty("Content-Type","application/json");
             }
-            binding.listView1.adapter = PersonAdapter(this, personList)
-        } catch (ee: JSONException) {
-            ee.printStackTrace()
+            conn.requestMethod = "GET"
+            response = convertStreamToString(BufferedInputStream(conn.inputStream))
+        } catch (e: MalformedURLException) {
+            Log.e(TAG, "MalformedURLException: " + e.message)
+        } catch (e: ProtocolException) {
+            Log.e(TAG, "ProtocolException: " + e.message)
+        } catch (e: IOException) {
+            Log.e(TAG, "IOException: " + e.message)
+        } catch (e: Exception) {
+            Log.e(TAG, "Exception: " + e.message)
         }
+        return response
     }
+
+    private fun convertStreamToString(`is`: InputStream): String {
+        val reader = BufferedReader(InputStreamReader(`is`))
+        val sb = StringBuilder()
+        var line: String?
+        try {
+            while (reader.readLine().also { line = it } != null) {
+                sb.append(line).append('\n')
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            try {
+                `is`.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        return sb.toString()
+    }
+
 }
--- app/src/main/java/com/example/mad_practical_10_21012011052/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011052/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mad_practical_10_21012011052
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
@@ -9,6 +8,9 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +18,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         val floatingBt = findViewById<FloatingActionButton>(R.id.floatingBt)
+
         floatingBt.setOnClickListener {
+
             CoroutineScope(Dispatchers.IO).launch {
                 try {
                     val data = HttpRequest().makeServiceCall(
                         "https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
-                        "dchj8v1b6qqdjzbqood1jgpachyfzlw58r540gru"
+                        "rbn0rerl1k0d3mcwgw7dva2xuwk780z1hxvyvrb1"
                     )
                     withContext(Dispatchers.Main) {
                         try {
@@ -37,21 +43,27 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
-
-        fun setArraytoListView() {
-            val personListView = findViewById<ListView>(R.id.Listview1)
-            val Array = arrayListOf<com.example.mad_practical_10_21012011052.Person>(
-                Person("1", "Smit", "nklnkl", "8511683205", "nllkl", 90.90, 909.90),
-                Person("2", "dvvdd", "nklnkl", "nklnkl", "nllkl", 90.90, 909.90),
-                Person("3", "nldvsdnk", "nklnkl", "nklnkl", "nllkl", 90.90, 909.90),
-                Person("4", "nldvdvnk", "nklnkl", "nklnkl", "nllkl", 90.90, 909.90),
-                Person("5", "scsnlnk", "nklnkl", "nklnkl", "nllkl", 90.90, 909.90)
-            )
-            personListView.adapter = PersonAdapter(this, Array)
-        }
     }
 
 
+    private fun getPersonDetailsFromJson(sJson: String) {
+        val personList = ArrayList<Person>()
+        try {
+            val jsonArray = JSONArray(sJson)
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray[i] as JSONObject
+                val person = Person(jsonObject)
+                personList.add(person)
+            }
+            val Personlistviewe = findViewById<ListView>(R.id.Listview1)
+            Personlistviewe.adapter = PersonAdapter(this, personList)
+        } catch (ee: JSONException) {
+            ee.printStackTrace()
+        }
+    }
 }
+
+
+
+
 
